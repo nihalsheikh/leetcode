01175363a34094b91384f58e64060ed0092f47ff
nihal <nihalsheikh585@gmail.com>
2025-12-07 08:07:51 +0530

    SOLVED: 238 Product of Array Elements except self

--- 006_product-of-array-except-self/Product_of_Array.py
+++ 006_product-of-array-except-self/Product_of_Array.py
@@ -1,8 +1,23 @@
 def products_of_elements(nums):
-	...
+	res = [1] * len(nums)
+	pre = 1
+	post = 1
+
+	for i in range(len(nums)):
+		res[i] = pre
+		pre *= nums[i]
+
+	n = len(nums) - 1
+	while n >= 0:
+		res[n] *= post
+		post *= nums[n]
+		n -= 1
+
+	return res
 
 def main():
-	...
+	nums = [1, 2, 3, 4]
+	print(products_of_elements(nums))
 
 
 if __name__ == "__main__":
